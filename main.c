eb6756985a96c6cf86d935262c8078553e1c3fd6
nardaiv <nardaikiru@gmail.com>
2024-11-18 14:50:03 +0700

    looping deathloop

--- main.c
+++ main.c
@@ -21,23 +21,27 @@ void battle(float *currentLearningPoints, int *userHP){
     char choice;
 
     printf("Subaru HP: %d | Elsa HP: %d", *userHP, elsaHP);
-    printf("Pilih aksi:\nA: Serang\nD: Menghindar\nR: Kabur (Reset ke checkpoint)");
+    printf("Pilih aksi:\nA: Serang\nD: Menghindar\nR: Kabur (Reset ke checkpoint)\n");
     printf("Pilihan: ");
     scanf("%c \n", &choice);
 
 }
 
 
-void deathLoop(int *loop){
-    float currentLearningPoints = 10;
+void deathLoop(int *deathCount, float *currentLearningPoints, bool *winningState){
+    
     int userHP = 100;
     int turnCount = 0;
     bool reset = false;
 
-    while(userHP > 0 ){
-        battle(&currentLearningPoints, &userHP);
+    printf("[Death Loop #%d]", *deathCount);
+
+    while(userHP > 0 || !winningState ){
+        battle(currentLearningPoints, &userHP);
         turnCount++;
     }
+
+
 
 }
 
@@ -46,12 +50,17 @@ void analysis(){
 }
 
 int main() {
+  float currentLearningPoints = 10;
+  bool winningState = false;
   printf("=== Return by Death Battle Simulator ===\n");
   printf("Subaru vs Elsa Granhiert\n\n");
 
 
   int deathCount = 1;
-  deathLoop(&deathCount);
+  while(deathCount<=10 || !winningState){
+    deathLoop(&deathCount, &currentLearningPoints, &winningState);
+    deathCount++;
+  }
 
   return 0;
 }
