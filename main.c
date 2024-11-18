1bfc7ba9d107d8598c1826cc69c7a1021193a2c1
nardaiv <nardaikiru@gmail.com>
2024-11-18 14:30:45 +0700

    creating some basic file structure

--- main.c
+++ main.c
@@ -1,18 +1,43 @@
 #include <stdio.h>
+#include <stdbool.h>
+
 
 #define ELSA_BASE_HP 200
 
-struct elsa
+struct loopReport
 {
-    /* data */
+    int damageReceive;
+    int damageDealt;
+    int turnCount;
+    char *deathCause;
+    int learningPoints;
 };
+
+
+void battle(float *currentLearningPoints, int *userHP){
+    int hpMultiplier = 2.0f - (*currentLearningPoints / 100.0f);
+    int elsaHP = (int)(ELSA_BASE_HP * hpMultiplier);;
+    
+    char choice;
+
+    printf("Subaru HP: %d | Elsa HP: %d", *userHP, elsaHP);
+    printf("Pilih aksi:\nA: Serang\nD: Menghindar\nR: Kabur (Reset ke checkpoint)");
+    printf("Pilihan: ");
+    scanf("%c \n", &choice);
+
+}
 
 
 void deathLoop(int *loop){
     float currentLearningPoints = 10;
-    float hpMultiplier = 2.0f - (currentLearningPoints / 100.0f);
-    float elsaHP = (int)(ELSA_BASE_HP * hpMultiplier);;
-    float userHP = 100;
+    int userHP = 100;
+    int turnCount = 0;
+    bool reset = false;
+
+    while(userHP > 0 ){
+        battle(&currentLearningPoints, &userHP);
+        turnCount++;
+    }
 
 }
 
@@ -21,8 +46,8 @@ void analysis(){
 }
 
 int main() {
-  printf("=== Return by Death Battle Simulator ===\n\n");
-  printf("Subaru vs Elsa Granhiert\n");
+  printf("=== Return by Death Battle Simulator ===\n");
+  printf("Subaru vs Elsa Granhiert\n\n");
 
 
   int deathCount = 1;
